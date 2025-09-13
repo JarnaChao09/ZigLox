1e24d9c91857c8ffac767ea3286d364ea666d9e4
JarnaChao09 <yunyang4928@gmail.com>
2025-09-12 22:35:57 -0400

    Small typo fix TODO: - Chapter 22: Local Variables - Update OpCode.asString to format - Update to hold writer types in structs   - Refactor printing to be output agnostic and use writer types - Refactor Object to hold equality check and printing

--- src/table.zig
+++ src/table.zig
@@ -32,28 +32,28 @@ pub const Table = struct {
         self.backing_map.deinit();
     }
 
-    const AdpatedCtx = struct {
+    const AdaptedCtx = struct {
         captured_chars: []const u8,
         captured_hash: u32,
 
-        pub fn init(chars: []const u8, chars_hash: u32) AdpatedCtx {
-            return AdpatedCtx{
+        pub fn init(chars: []const u8, chars_hash: u32) AdaptedCtx {
+            return AdaptedCtx{
                 .captured_chars = chars,
                 .captured_hash = chars_hash,
             };
         }
 
-        pub fn hash(self: AdpatedCtx, _: @TypeOf(void)) u64 {
+        pub fn hash(self: AdaptedCtx, _: @TypeOf(void)) u64 {
             return self.captured_hash;
         }
 
-        pub fn eql(self: AdpatedCtx, _: @TypeOf(void), key: *ObjString) bool {
+        pub fn eql(self: AdaptedCtx, _: @TypeOf(void), key: *ObjString) bool {
             return self.captured_hash == key.*.hash and std.mem.eql(u8, self.captured_chars, key.*.chars);
         }
     };
 
     pub fn findString(self: *Table, chars: []const u8, chars_hash: u32) ?*ObjString {
-        return self.backing_map.getKeyAdapted(void, AdpatedCtx.init(chars, chars_hash));
+        return self.backing_map.getKeyAdapted(void, AdaptedCtx.init(chars, chars_hash));
     }
 
     pub fn set(self: *Table, key: *ObjString, value: Value) bool {
